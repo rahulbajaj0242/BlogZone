70c7169ab7122b77bd6892a838a72fa9d71d2f02
Rahul Bajaj <rahulbajaj0242@gmail.com>
2022-11-22 21:06:56 -0500

    added auth-service.js

---
+++ auth-service.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  email: String,
+  loginHistory: [{ dateTime: Date, userAgent: String }],
+});
+
+let Company = mongoose.model('web322_companies', companySchema);
