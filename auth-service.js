651c4391b837c0e79478ac9d5136491becdc18dc
Rahul Bajaj <rahulbajaj0242@gmail.com>
2022-11-28 15:59:12 -0500

    + registerUser and checkUser in auth-service.js

--- auth-service.js
+++ auth-service.js
@@ -12,9 +12,10 @@ var userSchema = new Schema({
 });
 
 // let Company = mongoose.model('web322_companies', companySchema);
+let User;
 
-module.exports.initialize = function () {
-  return new Promise(function (resolve, reject) {
+module.exports.initialize = () => {
+  return new Promise((resolve, reject) => {
     let db = mongoose.createConnection(
       'mongodb+srv://dbUser:hnu6yxq-hjd-bht4JZU@senecaweb.xbzg820.mongodb.net/?retryWrites=true&w=majority'
     );
@@ -25,5 +26,61 @@ module.exports.initialize = function () {
       User = db.model('users', userSchema);
       resolve();
     });
+  });
+};
+
+module.exports.registerUser = (userData) => {
+  return new Promise((resolve, reject) => {
+    if (userData.password != userData.password2) {
+      reject('Passwords do not match');
+    } else {
+      let newUser = new User(userData);
+      newUser
+        .save()
+        .then(() => {
+          resolve();
+        })
+        .catch((err) => {
+          if (err.code == 11000) {
+            reject('User Name already taken');
+          } else {
+            reject('There was an error creating the user: ' + err);
+          }
+        });
+    }
+  });
+};
+
+module.exports.checkUser = (userData) => {
+  return new Promise((resolve, reject) => {
+    User.find({ userName: userData.userName })
+      .exec()
+      .then((users) => {
+        if (users.length === 0) {
+          reject('Unable to find user: ' + userData.userName);
+        }
+        if (users[0].password != userData.password) {
+          reject('Incorrect Password for user: ' + userData.userName);
+        } else {
+          users[0].loginHistory.push({
+            dateTime: newDate().toString(),
+            userAgent: userData.userAgent,
+          });
+          User.updateOne(
+            { userName: users[0].userName },
+            { $set: { loginHistory: users[0].loginHistory } }
+          )
+            .exec()
+            .then(() => {
+              resolve(users[0]);
+            })
+            .catch((err) => {
+              reject('There was an error verifying the user: ' + err);
+            });
+        }
+      })
+      .catch(() => {
+        reject('Unable to find user:  ' + userData.userName);
+      });
   });
 };
